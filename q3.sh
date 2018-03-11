22031f09a3f92adb25f141aab35a9934c03fb673
ArslanAhmedMir <32216624+ArslanAhmedMir@users.noreply.github.com>
2018-03-11 19:08:46 +0500

    ASSIGNMENT_1_BCSF15M557

--- q3.sh
+++ q3.sh
@@ -1,54 +1,37 @@
-#! /bin/sh
-
-UNIX=(Debian "REd hat" Ubuntu Suse Fedora)
-#Displaying all elememts
-echo "Elements of the array :"${UNIX[@]}
-
-#Displaying length of the array
-echo "Length   of the array :"${#UNIX[@]}
-
-#Displaying legth of specific element
-echo "Length   of the 2nd element of array :"${#UNIX[2]}
-
-#Extrating specific elements from the array
-echo "Extracting 2 elements from 3rd :"${UNIX[@]:3:4}
-
-#SEraching and replacing
-echo "Replacing ubuntu with hello :" ${UNIX[@]/Ubuntu/Hello}
-
-#Adding elements in the array
-UNIX=(${UNIX[@]} Aix HP-UX)
-echo "Elements of the array :"${UNIX[@]}
-
-#Removing the 3rd element
-unset UNIX[3]
-echo "Elements of the array :"${UNIX[@]}
-
-#Copying an array in UNIX
-LINUX=(linux)
-LINUX=( ${LINUX[@]} ${UNIX[@]} )
-echo "Elements of the Linux :"${LINUX[@]}
-
-#Copying both arrays in Bash
-BASH=( ${LINUX[@]} ${UNIX[@]} )
-echo "Elements of the Bash :"${BASH[@]}
-
-#Removing arrays UNIX LINUX
-unset UNIX[@]
-unset LINUX[@]
-echo "Elements of the UNIX :"${UNIX[@]}
-echo "Elements of the Linux :"${LINUX[@]}
+#!/bin/bash
+file=$1
+counter=0
 
  
+lower_case(){
+	echo "enter string to convert into lower case"
+	read str
+	str=${str,,}
+	echo "lower case string:"$str
+	}
+	
+check_root(){
+				if [ `whoami` != 'root' ]
+				then
+					echo "You are not root."
+				
+				fi
+	}
+	
+check_user(){
+	
+			echo "Enter user name to see if exist"
+			read user
+		    id=" `grep -c $user /etc/passwd `"
 
+			if [ $id=1 ]
+			then
+					echo "user exist"
+			else
+					echo "user does not exist"
+			fi
+	}
 
-
-
-
-
-
-
-
-
-
-
+lower_case
+check_root
+check_user
--- q4.sh
+++ q4.sh
@@ -1,5 +1,18 @@
-#! /bin/sh
+#!/bin/bash
+file=$1
+counter=0
 
-a=(`cat f1.txt`)
-echo ${a[@]}
+ 
+if [ $# -ne 1 ]
+then
+	echo "$0 fileName"
+	exit 1
+fi
 
+if [ ! -f $file ]
+then
+	echo "$file not found /not a file"
+	exit 2
+fi
+
+echo `sort $file | uniq -u | tee $file`
